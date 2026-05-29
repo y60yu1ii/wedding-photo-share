ea90b2ed064579aa20f014a0733e8f020e492fca
yaoyu <yaoyuli@pqdusa.com>
2026-05-29 17:35:27 +0800

    chore(build): update stack build artifact

--- dist/lib/wedding-photo-stack.js
+++ dist/lib/wedding-photo-stack.js
@@ -159,6 +159,8 @@ class WeddingPhotoStack extends cdk.Stack {
                 allowMethods: [
                     apigwv2.CorsHttpMethod.GET,
                     apigwv2.CorsHttpMethod.POST,
+                    apigwv2.CorsHttpMethod.PUT,
+                    apigwv2.CorsHttpMethod.PATCH,
                     apigwv2.CorsHttpMethod.DELETE,
                     apigwv2.CorsHttpMethod.OPTIONS,
                 ],
